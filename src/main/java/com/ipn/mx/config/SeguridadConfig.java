aa292d65fa667e412c6bf06767606c5d49065e9c
EnriqueCons <enri_cons23@outlook.com>
2025-07-01 00:10:07 -0600

    ultimo cambio de contraseña

--- src/main/java/com/ipn/mx/config/SeguridadConfig.java
+++ src/main/java/com/ipn/mx/config/SeguridadConfig.java
@@ -1,13 +1,27 @@
 package com.ipn.mx.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SeguridadConfig {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(Customizer.withDefaults()) // <-- usa el customizer, pero puedes omitirlo si no usas CSRF
+                .build();
     }
 }
